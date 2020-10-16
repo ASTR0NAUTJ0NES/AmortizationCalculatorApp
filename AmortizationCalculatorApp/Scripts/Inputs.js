dc2f99fae3ddeaf57a0fe50be16cd0840acba85d
Anthony D <CypharX@gmail.com>
2020-10-16 10:35:59 -0400

    input validation

---
+++ AmortizationCalculatorApp/Scripts/Inputs.js
@@ -0,0 +1,33 @@
+﻿document.getElementById('inputTerm').addEventListener('keypress', function (evt) { 
+    let char = evt.which;
+    
+    if (char >= 48 && char <= 57 ) {
+        return true;
+    }
+    else {
+        evt.preventDefault();
+        return false;
+    }
+});
+document.getElementById('inputLoan').addEventListener('keypress', function (evt) {
+    let char = evt.which;
+
+    if (char >= 48 && char <= 57) {
+        return true;
+    }
+    else {
+        evt.preventDefault();
+        return false;
+    }
+});
+document.getElementById('inputInterest').addEventListener('keypress', function (evt) {
+    let char = evt.which;
+
+    if (char >= 48 && char <= 57) {
+        return true;
+    }
+    else {
+        evt.preventDefault();
+        return false;
+    }
+});
